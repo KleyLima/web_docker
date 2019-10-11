dd352b994eb61e84f658de23d269fd31206665e5
Kleyton <kleyton.nascimento@semantix.com.br>
2019-10-11 05:23:31 -0300

    HomePage e PageResult

---
+++ source/wrapper/pages/query_cpf.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+class QueryCPF:
+
+    @classmethod
+    def __init__(cls):
+        cls.html=\
+                """
+                <!DOCTYPE html>
+		<html lang='pt'>
+	<head>
+		<meta charset="UTF-8">
+		<title>Home Page</title>
+		<div class = "backg">
+			<link rel="stylesheet" href="pages/style.css">
+		</div>
+	</head>
+	<body>
+        	<h1>
+        	Web Docker
+		</h1><br>
+
+		<h2>
+		Resultado:
+		</h2><br><br>
+		
+        	<div id="jinja2-div">
+            		
+		</div>
+
+	</body>
+</html>              
+                """
+
--- source/wrapper/webservice.py
+++ source/wrapper/webservice.py
@@ -3,6 +3,8 @@
 from aiohttp import web
 import json
 from source.service.people_service import PeopleService
+from jinja2 import Environment, PackageLoader, select_autoescape
+
 
 
 class Handler:
@@ -17,9 +19,14 @@ class Handler:
         port 7979
         """
         app = web.Application()
-        app.add_routes([web.post('/consulta_dtnasc/', self.post)])
-        # app.router.add_static('/pages/', path='pages/',
-        #                       append_version=False)  # Adiciona rota 'interna' para arquivos dentro do projeto.
+        app.add_routes([web.get('/', self.home_page),
+                        web.post('/consulta_dtnasc/', self.post)])
+        app.router.add_static('/pages/', path='pages/',
+                              append_version=False)
+        app.router.add_static('/consulta_dtnasc/pages', path='pages/',
+                              append_version=False)
+        self.env = Environment(loader=PackageLoader('webservice', 'pages'),
+                               autoescape=select_autoescape(['html']))
         web.run_app(app, host='localhost', port=7979)
 
     async def post(self, request):
@@ -27,7 +34,15 @@ class Handler:
         cpf = data.get('cpf')
         note = PeopleService().check_cpf(cpf=cpf)
         note = json.dumps(note)
-        return web.Response(status=200, body=note, content_type='application/json')
+        page = self.env.get_template('index1.html')
+        out = page.render(nota=note)
+        return web.Response(text=out, content_type='html')
+
+    async def home_page(self, request):
+        """ Método da homepage do serviço."""
+        page = self.env.get_template('index.html')
+        out = page.render()
+        return web.Response(text=out, content_type='html')
 
 
 if __name__ == '__main__':
