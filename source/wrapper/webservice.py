ccc0119fd6126665985b4efea4db165e6007e857
Kleyton <kley158@gmail.com>
2019-10-18 00:25:25 -0300

    AJustes para versão sem página web

--- source/wrapper/webservice.py
+++ source/wrapper/webservice.py
@@ -18,8 +18,7 @@ class Handler:
         port 7979
         """
         app = web.Application()
-        app.add_routes([web.get('/', self.home_page),
-                        web.get('/consulta_dtnasc/{cpf}', self.get_cpf),
+        app.add_routes([web.get('/consulta_dtnasc/{cpf}', self.get_cpf),
                         web.post('/consulta_dtnasc/', self.post_cpf)])
         app.router.add_static('/pages/', path='pages/', append_version=False)
         app.router.add_static('/consulta_dtnasc/pages', path='pages/', append_version=False)
@@ -28,23 +27,12 @@ class Handler:
 
     async def get_cpf(self, request):
         cpf = request.match_info['cpf']
-        #return web.Response(text=PeopleService().check_cpf(cpf=cpf), content_type='text/plain')
         return web.json_response(PeopleService().check_cpf(cpf=cpf))
 
     async def post_cpf(self, request):
-        data = await request.post()
-        cpf = data.get('cpf')
-        asw = PeopleService().check_cpf(cpf=cpf)
-        page = self.env.get_template('resposta.html')
-        out = page.render(resp=asw)
-        return web.Response(text=out, content_type='html')
-
-    async def home_page(self, request):
-        """ Método da homepage do serviço."""
-        page = self.env.get_template('index.html')
-        out = page.render()
-        return web.Response(text=out, content_type='html')
-
+        data = await request.json()
+        cpf=data.get('cpf') 
+        return web.json_response(PeopleService().check_cpf(cpf=cpf))
 
 if __name__ == '__main__':
     Handler()
